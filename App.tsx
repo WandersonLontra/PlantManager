15f23fed1e3fd2ac84d647687a7275c93e65f165
WandersonLontra <wandersonlontra@yahoo.com.br>
2021-04-19 23:05:06 -0300

    Finalizaro dia 1

--- App.tsx
+++ App.tsx
@@ -1,3 +1,17 @@
+import React from 'react';
+import { Welcome } from './src/pages/Welcome';
+
+export default function App() {
+  return (
+    <Welcome />
+  );
+}
+
+
+
+
+
+/*
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
@@ -5,7 +19,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello Wanderson</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -19,3 +33,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+*/
